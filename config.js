27031868871af21627b7c640441348605b75ab69
Anschluss-zeit <111547592+Anschluss-zeit@users.noreply.github.com>
2023-03-11 15:16:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const authKey = "d464AyXF5B4lrwA7Lkw89%2FzMryhf7k08CaQY05G7mHVgYj1OxgCPfc%2Fo3fHIzKFgtkWHx7YPtfNsrVB1sXPJze7zz6k1"
+const authKey = "   "
 //填写ZxYQ_8cea_auth
-const saltKey = "D6YJFYz9"
+const saltKey = "   "
 //填写ZxYQ_8cea_saltkey
 const timeout = 1 * 3600
 //每次请求的间隔时间，单位秒
